20a7a9d6aa618f44037a620c4a2c3573f9a1e9a1
andreluizsilv <109443784+andreluizsilv@users.noreply.github.com>
2022-11-11 19:29:56 -0300

    Aplicando Bootstrap no forms

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
@@ -7,12 +7,13 @@ class FormCriarConta(FlaskForm):
     username = StringField('Nome do Usuário', validators=[DataRequired()])
     email =StringField('E-mail', validators=[DataRequired(), Email()])
     senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
-    confimacao = PasswordField('Confimação da Senha', validators=[DataRequired(), EqualTo('senha')])
+    confimacao_senha = PasswordField('Confimação da Senha', validators=[DataRequired(), EqualTo('senha')])
     botao_submit_criarconta = SubmitField('Criar Conta')
 
 
 class FormLogin(FlaskForm):
     email = StringField('E-mail', validators=[DataRequired(), Email()])
     senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
-    botao_submit_logim = SubmitField('Fazer Login')
+    lembrar_dados = BooleanField('Lembrar dados de Acesso')
+    botao_submit_login = SubmitField('Fazer Login')
 
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, url_for
+from forms import FormCriarConta, FormLogin
 
 app = Flask(__name__)
 
 lista_usuarios = 'Lira', 'João', 'Alon', 'Alessandra', 'Amanda'
+
+app.config['SECRET_KEY'] = '57533e07a7431b7bbeb5a063f8ae2eca'
 
 @app.route("/")
 def home():
@@ -22,7 +25,9 @@ def usuarios():
 
 @app.route("/login")
 def login():
-    return render_template('login.html')
+    form_login = FormLogin()
+    form_criarconta = FormCriarConta()
+    return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
 
 
 
